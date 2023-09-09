9cf5f86cb13ab7818454d5a326edaf39bf643e60
Gabriel Gomez <gabrieleduardogomezgago@gmail.com>
2023-09-09 14:35:54 -0300

    instalacion de modulo express

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
-// importar modulo fs
-const fs = require(`fs`);
+// // importar modulo fs
+// const fs = require(`fs`);
 
-// //creacion de Archivo txt a traves de "fs"
-// fs.createWriteStream(`./archivo.txt`, (err, done)=>{
-//     console.log(`Archivo creado con Exito`);
-// });
+// // //creacion de Archivo txt a traves de "fs"
+// // fs.createWriteStream(`./archivo.txt`, (err, done)=>{
+// //     console.log(`Archivo creado con Exito`);
+// // });
+
+
 
 // importar modulo http
 const http = require(`http`);
+const { clearScreenDown } = require("readline");
+
+const users = [{
+    id: 1,
+    name: "john",
+    lastname: "Doe"
+},
+{
+    id: 2,
+    name: "Tony",
+    lastname: "Stark"
+},
+{
+    id: 3,
+    name: "Laura",
+    lastname: "Smith"
+}, ]
+
+
+
+
 
 //Crear servidor con "http"
 http.createServer( (req, res) => {
-    res.write(req.url)
-    res.end()
-}) .listen(3000, console.log(`El servidor está funcionando en https://localhost:3000`))
+
+    if(req.url === `/users`){
+        console.log(req.url);
+        // (200, { 'Content-Type': 'text/html' }); 
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify(users));
+        res.end();
+        return;
+    }
+
+    res.end() 
+}) .listen(3000, console.log(`El servidor está funcionando en http://localhost:3000/`))
